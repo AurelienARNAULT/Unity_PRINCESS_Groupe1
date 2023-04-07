c13a6781cc56aeefe688365ee1bc8d67d2a7299b
Weel06 <60885609+weelBenAissa@users.noreply.github.com>
2023-04-07 12:31:58 +0200

    Update Musée

--- Assets/Weel/cam.cs
+++ Assets/Weel/cam.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class cam : MonoBehaviour
 {
-    public float moveSpeed = 10f; // Vitesse de déplacement de la caméra
-    public float rotateSpeed = 100f; // Vitesse de rotation de la caméra
+    public float moveSpeed = 1f; // Vitesse de déplacement de la caméra
+    public float rotateSpeed = 20f; // Vitesse de rotation de la caméra
+
+    private float distanceFromGround; // Distance de la caméra par rapport au sol
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 2, -10);
+        // Calcul de la distance entre la caméra et le sol
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, Mathf.Infinity))
+        {
+            distanceFromGround = hit.distance;
+        }
     }
 
     void Update()
     {
         // Déplacement horizontal de la caméra
         float horizontal = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(horizontal, 0, 0) * moveSpeed * Time.deltaTime);
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+        moveDirection = transform.TransformDirection(moveDirection);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-        float translation = Input.GetAxis("Vertical") * moveSpeed;
-        translation *= Time.deltaTime;
-        transform.Translate(0, 0, translation);
+        // Correction de la hauteur de la caméra
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, Mathf.Infinity))
+        {
+            float groundDistance = hit.distance;
+            float heightCorrection = distanceFromGround - groundDistance;
+            transform.position += new Vector3(0, heightCorrection, 0);
+        }
 
-        /*// Rotation de la caméra
-        float rotate = Input.GetAxis("Rotate");
-        transform.Rotate(new Vector3(0, rotate, 0) * rotateSpeed * Time.deltaTime);
-        */
+        // Rotation de la caméra
+        float rotate = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+        transform.Rotate(0, rotate, 0);
     }
 }
